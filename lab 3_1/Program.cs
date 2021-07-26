ffe6c655f24a15dc894f079efcbd7f4a1d20cacd
Jeff Metzner <jeffmetzner@quickenloans.com>
2021-07-26 17:28:20 -0400

    3dd version!

--- lab 3_1/Program.cs
+++ lab 3_1/Program.cs
@@ -6,16 +6,15 @@ namespace lab_3_1
     {
         static bool KeepGoing()
         {
-            // Version 0.04. (Agile Programming)
+            
             while (true)
             {
-                // Print out message asking if user wants to continue
+                
                 Console.WriteLine("Would you like to go again? (y/n)");
-                // Ask the user for their input
+               
                 string response = Console.ReadLine();
                 response = response.ToLower();
-                // Check if they typed "y". If so, return true.
-                // Otherwise return false.   YES   yes
+                
                 if (response == "y" || response == "yes")
                 {
                     return true;
